6bcf004c3b49e9456f0cb8a6e527e6d941e78420
Ijoelel <fianarnatha13@gmail.com>
2024-11-28 13:58:30 +0700

    first commit

---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const sleep = (ms: number): Promise<NodeJS.Timeout> =>
+    new Promise((resovle) => setTimeout(resovle, ms));
+
+export const getRandom = <T>(arr: T[]): T =>
+    arr[Math.floor(Math.random() * arr.length)];
